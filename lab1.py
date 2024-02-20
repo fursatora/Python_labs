d91e35fc3d1081860ef72ed470b3ec384f5521f0
Виктория Фурса <vikahomeacc@gmail.com>
2024-02-20 21:32:27 +0300

    Задание 4. №18.
    Найти в тексте даты формата «день.месяц.год».

--- lab1.py
+++ lab1.py
@@ -81,4 +81,19 @@ else:
     print("НЕ палиндром.")
 
 
+#18 Найти в тексте даты формата «день.месяц.год».
+import re
+def find_dates(text):
+    pattern = r'\b(\d{1,2}\.\d{1,2}\.\d{4})\b'
+    dates = re.findall(pattern, text)
+    return dates
+
+if find_dates(text):
+    for date in find_dates(text):
+        print(date)
+else:
+    print("Формата «день.месяц.год» НЕТ.")
+
+
+
 
