e807b5f18600496cbdcc5a70af34113fa968ba7d
Виктория Фурса <vikahomeacc@gmail.com>
2024-02-20 21:18:37 +0300

    Задание 2. №9.
    Дана строка. Необходимо проверить образуют ли строчные символы
    латиницы палиндром.

--- lab1.py
+++ lab1.py
@@ -71,6 +71,14 @@ def count_russian_characters(text):
 print("Количество русских символов в тексте:", count_russian_characters(text))
 
 
+#9 Дана строка. Необходимо проверить образуют ли строчные символы латиницы палиндром.
+def is_palindrome(s):
+    s = ''.join([c.lower() for c in s if c.isalpha()])
+    return s == s[::-1]
+if is_palindrome(text):
+    print("Палиндром.")
+else:
+    print("НЕ палиндром.")
 
 
 
